958b5638a8cd568b602f199f6086aac383848464
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-06-30 09:41:27 +0530

    Day2

---
+++ Typescript/Day2/demo1.ts
@@ -0,0 +1,35 @@
+console.log("Hello World...");
+console.log('Welcome to My Angular Module');
+
+// identifier
+// do not start with digit
+// we can start with char
+// also we can use $ & _ 
+// No any special keyword & symbol is used
+// core java int a;
+var a;
+var a1;
+var a$b;
+var a2,b2;
+
+// Data Type
+// number(int,long,double,float,short),string(string,char),boolean(true/false),
+// any(number,string,boolean),void (It Does Not point any thing=> its netural datat)
+// Misec. => null & undefined
+
+var b1:number=10;   //forward declration & definition
+console.log('Value b1 is '+b1);
+b1=2.5;
+console.log(b1);
+
+var str:string;
+console.log("Inital value of str:: "+str);
+var b3:number;
+console.log("Number inital value is :: "+b3);
+
+var strany:any=3.5;
+console.log("value of strany is "+strany);
+strany='Hi..';
+console.log("value of strany is "+strany);
+strany=false;
+console.log("value of strany is "+strany);
