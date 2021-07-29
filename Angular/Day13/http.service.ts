1995b4943b90342548ee82c7b60fe95bfd82fe37
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-07-29 10:04:02 +0530

    Day13

---
+++ Angular/Day13/http.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+     private url:string="https://jsonplaceholder.typicode.com/posts";
+  constructor(private http:HttpClient) { }
+
+    getPosts(){
+      return (this.http.get(this.url));
+    }
+
+    
+
+}
